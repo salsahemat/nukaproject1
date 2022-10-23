d3e743ef7934d733597c0012bcf8aa75c521a1cb
Nuzulul-Salsabila <nsalsabila02@student.ciputra.ac.id>
2022-10-23 19:41:38 +0700

    i add pict

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -4,5 +4,5 @@
     <h1 class="text-center">{{ $maintitle }}</h1> <br>
     <p class="lh-lg text-center">Ohmly Barn is a dairy brand that has the <br> acronym (O)at (H)ome(m)ade(ly) (Barn). <br> 
         We provide the best gluten free food <br> withlactose intolerance.</p>
-    
+    <img src="../images/womanhike.png">
 @endsection
