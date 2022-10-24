8d6fa1df06cbe67a8b23fc0c7e6c6dc56b6658cc
Nuzulul-Salsabila <nsalsabila02@student.ciputra.ac.id>
2022-10-24 15:25:15 +0700

    i changed home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,7 +8,8 @@
         <h1 class="lh-base fs-1">{{ $maintitle }} <br>Homemade  Oatmilk</h1> <br>
         <p class="lh-base fs-4">Ohmly  Barn  is  made  from  simple,<br> natural  oats.  Plant-based,  vegan- <br> 
           friendly,  dairy  and  lactose-free</p> <br>
-        <button type="button" class="btn btn-outline btn-lg" style="background-color: #F2F5C8">Learn More</button>
+          <a href="/about" class="btn btn-outline btn-lg" style="background-color: #F2F5C8">Learn More</a>
+        {{-- <button  href="/about"  type="button" class="btn btn-outline btn-lg" style="background-color: #F2F5C8">Learn More</button> --}}
     </div>
       <div class="col-lg-6">
         <div class="row">
@@ -20,12 +21,6 @@
                ingrediants and we use no artificial colors </p>
 
             </div>
-            {{-- <div class="row"> --}}
-             {{-- <h2 class="mt-0 fs-1">Why Us?</h2>
-             <p class="mt-3 lh-base fs-4 mb-5">ohmly barn use natural sweetener <br> 
-              (Stevia), crafted with premium <br>
-               ingrediants and we use no artificial colors </p> --}}
-          {{-- </div> --}}
         </div>
       </div>
     </div>
