1dea6bdda4d3960d55a410222008937e57e92274
Nuzulul-Salsabila <nsalsabila02@student.ciputra.ac.id>
2022-10-24 15:18:18 +0700

    change product

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -5,18 +5,23 @@
     <h1 class="text-center">{{ $maintitle }}</h1>
     <div class="card mb-3" style="max-width: 540px;">
         <div class="row g-0">
-          <div class="col-md-4">
-            <img src="../images/Vanilalala.png" class="img-fluid rounded-start" alt="...">
-          </div>
-          <div class="col-md-8">
-            <div class="card-body">
-                @foreach ($products as $index => $product)
-                <h5 class="card-title">{{ $product['name'] }}</h5>
-                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-  
+            @foreach ($products as $product)
+                <div class="col-auto col-md-3 mx-auto mx-md-0">
+                    <div class="card my-2" style="width: 15rem">
+                        <img src="{{ asset("images/{$product['image_name']}") }}" class="card-img">
+                        <div class="card-body">
+                            <h5 class="card-title">{{ $product['name'] }}</h5>
+                            <p class="card-text">Ingredients: {{ $product['Ingredients'] }}</p>
+                            <div class="d-flex">
+                                <a href="/showproduct/{{ $product['id'] }}" class="btn btn-warning">Learn More</a>
+                                    {{-- <button href="/showproduct/{{ $product['id'] }}"></button> --}}
+                            </div>
+                        </div>
+                    </div>
+                </div>
             @endforeach
-            </div>
-          </div>
         </div>
-      </div>
+    </div> 
+    </div>
+    </div>
 @endsection
